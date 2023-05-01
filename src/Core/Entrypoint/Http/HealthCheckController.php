91e33b0dae44ccc2d8700f9e3c95b088fbf0a196
Raúl Almeida <personal@raulalmeida.com>
2023-05-01 10:12:09 +0200

    directory structure and controller configuration in service.yaml and HealthCheckController

--- src/Core/Entrypoint/Http/HealthCheckController.php
+++ src/Core/Entrypoint/Http/HealthCheckController.php
@@ -2,7 +2,24 @@
 
 declare(strict_types=1);
 
+namespace App\Core\Entrypoint\Http;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 final class HealthCheckController
 {
-
+    #[Route(
+        '/',
+        name: 'health.check',
+        methods: ['GET'],
+    )]
+    public function __invoke(): Response
+    {
+        return new JsonResponse(
+            ['status' => 'ok'],
+            Response::HTTP_OK,
+        );
+    }
 }
